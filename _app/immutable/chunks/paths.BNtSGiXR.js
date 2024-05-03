245abefcc47bf1f6e66ca011a392f6bff206fd06
Baspel <brianovkolp@gmail.com>
2024-05-03 16:43:27 +0200

    Updates

--- _app/immutable/chunks/paths.BNtSGiXR.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_suz29f)==null?void 0:s.base)??"/spookyrp.cz";var a;const t=((a=globalThis.__sveltekit_suz29f)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/chunks/paths.BVUlsm3W.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1dqhjtq)==null?void 0:s.base)??"/spookyrp.cz";var t;const e=((t=globalThis.__sveltekit_1dqhjtq)==null?void 0:t.assets)??a;export{e as a,a as b};
